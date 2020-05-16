d44147e7ed2bdca10e0219577a17396c978e281e
Vincent L <vincentwl1@hotmail.com>
2020-05-16 14:39:00 -0500

    Added Longhorn v3.0 Layout

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,23 +31,23 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
             this.button10 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Cursor = System.Windows.Forms.Cursors.Default;
-            this.button1.Location = new System.Drawing.Point(56, 282);
+            this.button1.Location = new System.Drawing.Point(45, 317);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(174, 23);
             this.button1.TabIndex = 0;
@@ -57,25 +57,16 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(56, 311);
+            this.button2.Location = new System.Drawing.Point(45, 346);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(174, 23);
             this.button2.TabIndex = 1;
             this.button2.Text = "Remove Default Apps";
             this.button2.UseVisualStyleBackColor = true;
             // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(56, 340);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(174, 29);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Disable Windows Defender";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(56, 375);
+            this.button4.Location = new System.Drawing.Point(45, 375);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(174, 23);
             this.button4.TabIndex = 3;
@@ -84,7 +75,7 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(56, 405);
+            this.button5.Location = new System.Drawing.Point(45, 404);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(174, 23);
             this.button5.TabIndex = 4;
@@ -93,7 +84,7 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(276, 282);
+            this.button6.Location = new System.Drawing.Point(45, 462);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(165, 23);
             this.button6.TabIndex = 5;
@@ -102,22 +93,12 @@
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(276, 328);
+            this.button7.Location = new System.Drawing.Point(45, 433);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(165, 23);
+            this.button7.Size = new System.Drawing.Size(201, 23);
             this.button7.TabIndex = 6;
             this.button7.Text = "Disable Windows Update";
             this.button7.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(525, 282);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(115, 15);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "Please run as admin!";
             // 
             // button8
             // 
@@ -158,22 +139,41 @@
             this.pictureBox1.TabIndex = 11;
             this.pictureBox1.TabStop = false;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(45, 492);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(174, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Disable Windows Defender";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 276);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.ClientSize = new System.Drawing.Size(784, 444);
+            this.ClientSize = new System.Drawing.Size(779, 537);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button10);
             this.Controls.Add(this.button9);
             this.Controls.Add(this.button8);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -190,16 +190,16 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button9;
         private System.Windows.Forms.Button button10;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -44,5 +44,15 @@ namespace LonghornX
         {
 
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
