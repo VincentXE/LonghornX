cfdd6b76af857c3b14724947dbf6e48a9b2deeae
Vincent L <vincentwl1@hotmail.com>
2020-05-16 09:44:33 -0500

    Added all needed buttons

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,6 +33,12 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -40,7 +46,7 @@
             this.button1.Cursor = System.Windows.Forms.Cursors.Default;
             this.button1.Location = new System.Drawing.Point(29, 355);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(125, 23);
+            this.button1.Size = new System.Drawing.Size(174, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Disable Cortana";
             this.button1.UseVisualStyleBackColor = true;
@@ -82,11 +88,72 @@
             this.button5.Text = "Block Telemetry";
             this.button5.UseVisualStyleBackColor = true;
             // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(249, 400);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(165, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Disable Intrusive Services";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(249, 469);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(165, 23);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Disable Windows Update";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(581, 355);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Please run as admin!";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(581, 384);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(135, 23);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "Setup Longhorn";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(581, 419);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(158, 23);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "Windows Information";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(581, 469);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(158, 23);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "Exit Longhorn X";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(821, 578);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
@@ -96,6 +163,7 @@
             this.Text = "LonghornX Alpha 0.1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -106,6 +174,12 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
     }
 }
 
